c493bd321c23ae6dd169d7a709a86199e35188e8
atharvyadav96k <atharvyadav96k@gmail.com>
2024-11-25 20:35:16 +0530

    google serach

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const productModel = require("./product")
 const userModel = require('./users');
 passport.use(new localStrategy(userModel.authenticate()));
 
-app.get('/sitemap.xml', (req, res) => {
+router.get('/sitemap.xml', (req, res) => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
